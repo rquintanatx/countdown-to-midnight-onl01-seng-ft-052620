a21809d68399f84dd191abbc0059aaf624d70261
Robert Quintana <robertquintanatx@gmail.com>
2020-05-28 00:31:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ def countdown(int)
   puts "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(int)
+  while int > 0 
+    puts "#{int} SECOND(S)!"
+    int -= 1 
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
+end
